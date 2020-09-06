ec8ea684731a0bcb11b4774051ae562683d55095
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-06 03:39:26 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -26,22 +26,28 @@ end
   # REMEMBER: This method **should** update cart
 
 def apply_clearance(cart)
-  cart.each do |hashes|
-    if hashes.value?(true)
-      hashes[:price] = (hashes[:price] * 0.8).round(2)
+  counter = 0
+  while counter < cart.length
+    if cart[counter][:clearance]
+      cart[counter][:price] = (cart[counter][:price] * 0.8).round(2)
       end
+      counter += 1
     end
     cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  consolidated_cart = consolidate_cart(cart)
+  applied_coupons = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(applied_coupons)
+  total_price = 0
+  counter = 0
+  while counter < final_cart.length
+    total_price += (final_cart[counter][:price] * final_cart[counter][:count])
+    counter += 1
+  end
+  if total_price > $100
+    total_price = (total_price * 0.9).round(2)
+  end
+  total_price
 end
