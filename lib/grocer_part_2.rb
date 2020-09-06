04a996a36cff344163acecdb84ab23cad51c6df1
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-06 01:19:46 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,10 +1,33 @@
 require_relative './part_1_solution.rb'
 
+require 'pry'
+
+# returns a new Array.
+# Its members will be a mix of the item Hashes and, where applicable, the "ITEM W/COUPON" Hash.
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  couponed_item = {}
+  cart << couponed_item
+  cart.each do |item_hash|
+  counter = 0
+  while counter < coupons.length
+      item_lookup = find_item_by_name_in_collection(coupons[counter][:item],cart)
+    if item_lookup && item_lookup[:count] >= coupons[counter][:num]
+      couponed_item[:item] = coupons[counter][:item] + " W/COUPON"
+      couponed_item[:price] = (coupons[counter][:cost] / coupons[counter][:num])
+      couponed_item[:clearance] = item_hash[:clearance]
+      couponed_item[:count] = item_hash[:count]
+      item_hash[:count] -= coupons[counter][:num]
+      # binding.pry
+      counter += 1
+      end
+    end
+  end
+  cart
 end
+
+
+  # REMEMBER: This method **should** update cart
+
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
